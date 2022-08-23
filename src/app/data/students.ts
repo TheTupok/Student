a33a17874a513f87b5ac350fab9c8e544be1bfb0
Anatoly Tereshonok <ter.tolya@mail.ru>
2022-08-23 20:41:05 +0300

    Adding a temporary database

---
+++ src/app/data/students.ts
@@ -0,0 +1,28 @@
+import {IStudent} from "../models/student";
+
+export const students: IStudent[] = [
+  {
+    "name": "125123",
+    "course": 3,
+    "_id": 1,
+    group: 'asdasg'
+  },
+  {
+    "name": "123",
+    "course": 1,
+    "_id": 2,
+    group: 'asdasg'
+  },
+  {
+    "name": "1232sd",
+    "course": 1,
+    "_id": 3,
+    group: 'asdasg'
+  },
+  {
+    "name": "SUKA",
+    "course": 5,
+    "_id": 4,
+    group: 'asss'
+  }
+];
